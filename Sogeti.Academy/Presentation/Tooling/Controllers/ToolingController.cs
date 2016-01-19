59d8e0c2226679460ff957552f4c033a7d1ac12e
Bryce Klinker <bryce.klinker@gmail.com>
2016-01-19 00:53:22 -0600

    Added tooling page to list tools that are used for site

---
+++ Sogeti.Academy/Presentation/Tooling/Controllers/ToolingController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace Sogeti.Academy.Presentation.Tooling.Controllers
+{
+    [Route("tooling")]
+    public class ToolingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
