8d84871ff590641feebed74d4c05819ec44f9bd1
Bryce Klinker <bryce.klinker@gmail.com>
2016-04-04 23:48:53 -0500

    Fixing if statements to have correct syntax.

--- deploy.cmd
+++ deploy.cmd
@@ -1,11 +1,7 @@
-if "%APP%"=="api"
-    call ".\src\dotnet\deploy-api.cmd"
+if /I "%APP%"=="api" call ".\src\dotnet\deploy-api.cmd"
     
-if "%APP%"=="mvc"
-    call ".\src\dotnet\deploy-mvc.cmd"
+if /I "%APP%"=="mvc" call ".\src\dotnet\deploy-mvc.cmd"
     
-if "%APP%"=="angular"
-    call ".\src\angular\deploy.cmd"
+if /I "%APP%"=="angular" call ".\src\angular\deploy.cmd"
 
-if "%APP%"=="react"
-    call ".\src\react\deploy.cmd"
+if /I "%APP%"=="react" call ".\src\react\deploy.cmd"
